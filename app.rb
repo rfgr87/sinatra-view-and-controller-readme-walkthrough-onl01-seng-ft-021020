90c22d0b9825eb00a1df9d3780afd1d401d5d125
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-24 16:31:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,9 @@ end
 
   get '/friends' do
     # Write your code here!
-    
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+ 
+  erb :friends
 
   end
 end
